29c80658373228aae905410ee5f8f6aa9c962703
amanandhar0 <amanandhar0@gmail.com>
2025-07-02 23:34:21 +0545

    fixed optimization issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,20 @@
 import './globals.css';
+import { Suspense } from 'react'
 
 export const metadata = {
   title: 'Anime Standoff',
   description: 'A card game',
 };
 
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Suspense fallback={<div>Loading...</div>}>
+          {children}
+        </Suspense>
+      </body>
     </html>
-  );
+  )
 }
